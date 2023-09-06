75058ac17c9b02577d4b3a263257bf19b32c46a0
TorchHollow <nikolasjuliogarciadejesus@gmail.com>
2023-09-06 14:44:30 -0300

    Fiz o slideshow da imagens

--- script.js
+++ script.js
@@ -7,4 +7,22 @@ function descerMenu() {
     } else {
         x.className = "titulo";
     }
-} 
+}
+
+/*Função do Slideshow*/
+
+let slideIndex = 0
+mostrarSlides()
+
+function mostrarSlides() {
+    let i
+    let slides = document.getElementsByClassName("meuSlide")
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"
+    }
+    slideIndex++
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block"
+    setTimeout(mostrarSlides, 4000)
+}
+
