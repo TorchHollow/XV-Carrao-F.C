f55ae7f872157e9c8f0c09286fa03537b8600d21
TorchHollow <nikolasjuliogarciadejesus@gmail.com>
2023-09-09 00:41:17 -0300

    Fiz a área dos proximos jogos

--- script.js
+++ script.js
@@ -21,8 +21,33 @@ function mostrarSlides() {
         slides[i].style.display = "none"
     }
     slideIndex++
-    if (slideIndex > slides.length) {slideIndex = 1}
-    slides[slideIndex-1].style.display = "block"
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block"
     setTimeout(mostrarSlides, 3000)
 }
+
+/*Função do timer*/
+
+let contagemData = new Date("2023-09-09 15:37:25").getTime()
+
+let contagemFinal = setInterval(function () {
+
+    let agora = new Date().getTime()
+
+    let distancia = contagemData - agora
+
+    let dias = Math.floor(distancia / (1000 * 60 * 60 * 24))
+    let horas = Math.floor((distancia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    let minutos = Math.floor((distancia % (1000 * 60 * 60)) / (1000 * 60))
+    let segundos = Math.floor((distancia % (1000 * 60)) / 1000)
+
+    document.getElementById("timer").innerHTML = dias + "d " + horas + "h "
+        + minutos + "m " + segundos + "s "
+
+    if (distancia < 0) {
+        clearInterval(contagemFinal)
+        document.getElementById("timer").innerHTML = "EXPIRADO"
+    }
+})
+
 
