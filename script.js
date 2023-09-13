44f3fd2fd1ed8a348ac7fe0185d60aab4a771c9a
TorchHollow <nikolasjuliogarciadejesus@gmail.com>
2023-09-12 21:31:32 -0300

    Fiz a área de camisetas e fiz outro slider

--- script.js
+++ script.js
@@ -50,4 +50,14 @@ let contagemFinal = setInterval(function () {
     }
 })
 
+/*Função do slider dos produtos*/
+
+let liEls = document.querySelectorAll('ul li')
+let index = 0
+window.show = function(increase){
+    index = index + increase
+    index = Math.min(Math.max(index,0), liEls.length-1)
+    liEls[index].scrollIntoView({behavior: 'smooth'})
+}
+
 
