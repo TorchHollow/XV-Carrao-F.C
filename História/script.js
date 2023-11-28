5411a34bd4837950cd4104eab3211a4737f7365a
Nikolas Júlio <nikolasjuliogarciadejesus@gmail.com>
2023-11-27 23:21:04 -0300

    Terminei aba de história

---
+++ História/script.js
@@ -0,0 +1,13 @@
+/*Função do menu mobile*/
+
+function descerMenu() {
+    let x = document.getElementById("meuTitulo");
+    if (x.className === "titulo") {
+        x.className += " responsive";
+    } else {
+        x.className = "titulo";
+    }
+}
+
+
+
