2084290c1d87cca8bc9bdebeda04385697d02260
Nikolas Júlio <nikolasjuliogarciadejesus@gmail.com>
2023-12-04 23:59:26 -0300

    Coloquei a mudanças no index principal.

--- script.js
+++ script.js
@@ -66,23 +66,4 @@ function fecharImagemCheia() {
     imagemCheiaBox.style.display = "none"
 }
 
-/*Função das Tabs*/
-
-function abrirAba(evt, nomeDaAba) {
-    let i, tabcontent, tablinks
-
-    tabcontent = document.getElementsByClassName("tabcontent")
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none"
-    }
-
-    tablinks = document.getElementsByClassName("tablinks")
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" ativo", "")
-    }
-
-    document.getElementById(nomeDaAba).style.display = "block"
-}
-document.getElementById("defaultOpen").click()
-
 
